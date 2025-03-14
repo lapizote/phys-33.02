a092d6c4d7780f95d985580550cbbcf6d7211358
Alexander Stephen Ulpindo <axlulpindo13@gmail.com>
2025-03-14 10:08:36 +0800

    fix model

--- lab7/40k.py
+++ lab7/40k.py
@@ -3,37 +3,8 @@ import math
 import matplotlib.pyplot as plt
 from sklearn.metrics import r2_score
 
-df_20k_chargeexp = pd.read_csv("20kohm-charge.csv")
-df_20k_dischargeexp = pd.read_csv("20kohm-discharge.csv")
 df_40k_chargeexp = pd.read_csv("40kohm-charge.csv")
 df_40k_dischargeexp = pd.read_csv("40kohm-discharge.csv")
-
-#20k charge
-theoretical_dict = {"Time (s)": [], "Voltage (V)": []}
-for x in df_20k_chargeexp["Time (s)"]:
-    time = float(x)
-    r = float(20000)
-    c = float(100*10**(-6))
-    e = float(20)
-    alpha = float(2)
-    v = float(e/alpha)*(1-math.exp(-(alpha*time)/(r*c)))
-    theoretical_dict["Time (s)"].append(time)
-    theoretical_dict["Voltage (V)"].append(v)
-df_20k_chargetheo = pd.DataFrame(data=theoretical_dict)
-print("Done theoretical 20k charge!")
-
-#20k discharge
-theoretical_dict = {"Time (s)": [], "Voltage (V)": []}
-for x in df_20k_dischargeexp["Time (s)"]:
-    time = float(x)
-    r = float(20000)
-    c = float(100*10**(-6))
-    e = float(10)
-    v = float(e)*(math.exp(-time/(r*c)))
-    theoretical_dict["Time (s)"].append(time)
-    theoretical_dict["Voltage (V)"].append(v)
-df_20k_dischargetheo = pd.DataFrame(data=theoretical_dict)
-print("Done theoretical 20k discharge!")
 
 #40k charge
 theoretical_dict = {"Time (s)": [], "Voltage (V)": []}
@@ -62,18 +33,6 @@ for x in df_40k_dischargeexp["Time (s)"]:
 df_40k_dischargetheo = pd.DataFrame(data=theoretical_dict)
 print("Done theoretical 40k discharge!")
 
-fig1 = plt.figure()
-fig1.canvas.manager.set_window_title("Charging, 20kOhms")
-ax = plt.subplot(111)
-df_20k_chargetheo.plot(kind = 'line', x = 'Time (s)', y = 'Voltage (V)', ax=ax, c="red", label="Theoretical")
-df_20k_chargeexp.plot(kind = 'scatter', x = 'Time (s)', y = 'Voltage (V)', ax=ax, c="blue", label="Experimental")
-
-fig2 = plt.figure()
-fig2.canvas.manager.set_window_title("Discharging, 20kOhms")
-ax = plt.subplot(111)
-df_20k_dischargetheo.plot(kind = 'line', x = 'Time (s)', y = 'Voltage (V)', ax=ax, c="red", label="Theoretical")
-df_20k_dischargeexp.plot(kind = 'scatter', x = 'Time (s)', y = 'Voltage (V)', ax=ax, c="blue", label="Experimental")
-
 fig3 = plt.figure()
 fig3.canvas.manager.set_window_title("Charging, 40kOhms")
 ax = plt.subplot(111)
@@ -86,12 +45,10 @@ ax = plt.subplot(111)
 df_40k_dischargetheo.plot(kind = 'line', x = 'Time (s)', y = 'Voltage (V)', ax=ax, c="red", label="Theoretical")
 df_40k_dischargeexp.plot(kind = 'scatter', x = 'Time (s)', y = 'Voltage (V)', ax=ax, c="blue", label="Experimental")
 
-print(f"R^2 for Charging, 20 kOhms: {r2_score(df_20k_chargeexp['Voltage (V)'], df_20k_chargetheo['Voltage (V)'])}")
-print(f"R^2 for Discharging, 20 kOhms: {r2_score(df_20k_dischargeexp['Voltage (V)'], df_20k_dischargetheo['Voltage (V)'])}")
-
 print(f"R^2 for Charging, 40 kOhms: {r2_score(df_40k_chargeexp['Voltage (V)'], df_40k_chargetheo['Voltage (V)'])}")
 print(f"R^2 for Discharging, 40 kOhms: {r2_score(df_40k_dischargeexp['Voltage (V)'], df_40k_dischargetheo['Voltage (V)'])}")
-
+print(df_40k_chargeexp)
+print(df_40k_chargetheo)
 plt.show()
 
 #print(df["Time (s)"])
